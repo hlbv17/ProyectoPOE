1f573b9eef4b46b7f5901a14615d808e938acce4
ropbGitHub <64661058+ropbGitHub@users.noreply.github.com>
2021-08-05 13:36:05 -0500

    Probando...

--- Proyecto_Dentalig/Persona.cs
+++ Proyecto_Dentalig/Persona.cs
@@ -3,16 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Model
-{
-    abstract class Persona
-    {
+namespace Model {
+    abstract class Persona {
         private char sexo;
         private string nombre;
         private DateTime fechaNac;
 
-        public Persona()
-        {
+        public Persona () {
             this.sexo = ' ';
             this.nombre = "";
             this.fechaNac = DateTime.Now;
@@ -22,14 +19,12 @@ namespace Model
         public string Nombre { get => nombre; set => nombre = value; }
         public DateTime FechaNac { get => fechaNac; set => fechaNac = value; }
 
-        public string leerSexo()
-        {
+        public string leerSexo () {
             return "";
         }
-        
-        public override string ToString()
-        {
-            return base.ToString();
+
+        public override string ToString () {
+            return base.ToString ();
         }
 
 
