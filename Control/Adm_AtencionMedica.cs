2fae9ab5ccc92f3a5e25dc79712dc1f035c07ae9
ropbGitHub <64661058+ropbGitHub@users.noreply.github.com>
2021-08-06 03:35:16 -0500

    Agregar adm

--- Control/Adm_AtencionMedica.cs
+++ Control/Adm_AtencionMedica.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Control
-{
-    public class Class1
-    {
+namespace Control {
+    class Adm_AtencionMedica {
     }
 }
