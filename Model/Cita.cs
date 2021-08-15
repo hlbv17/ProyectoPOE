4109d709c96b662ff0e0d1e36a3c35e36ac2bdc9
Helen Bernal <helenlbv17@gmail.com>
2021-08-15 18:49:23 -0500

    clase cita actualizada

--- Model/Cita.cs
+++ Model/Cita.cs
@@ -5,44 +5,47 @@ using System.Linq;
 using System.Text;
 
 namespace Proyecto_Dentalig {
-    class Cita {
+    class Cita
+    {
 
+        private int id_cita;
         private DateTime fecha;
         private DateTime hora;
-        private int consultorio;
         private Odontologo odontologo;
         private Paciente paciente;
 
-        public Cita () {
+        public Cita()
+        {
+            this.id_cita = 1;
             this.fecha = DateTime.Now;
             this.hora = DateTime.Now;
-            this.consultorio = 0;
             this.odontologo = null;
             this.paciente = null;
         }
 
-        public Cita (DateTime fecha, DateTime hora, int consultorio, Odontologo odontologo, Paciente paciente) {
+        public Cita(int id_cita, DateTime fecha, DateTime hora, Odontologo odontologo, Paciente paciente)
+        {
+            this.id_cita = id_cita;
             this.fecha = fecha;
             this.hora = hora;
-            this.consultorio = consultorio;
             this.odontologo = odontologo;
             this.paciente = paciente;
         }
 
         public DateTime Fecha { get => fecha; set => fecha = value; }
         public DateTime Hora { get => hora; set => hora = value; }
-        public int Consultorio { get => consultorio; set => consultorio = value; }
+        public int Id_cita { get => id_cita; set => id_cita = value; }
         internal Odontologo Odontologo { get => odontologo; set => odontologo = value; }
         internal Paciente Paciente { get => paciente; set => paciente = value; }
 
-        public override string ToString () {
+        public override string ToString()
+        {
             return
+                "\r\nPaciente: " + paciente.Nombre +
                 "\r\nFecha: " + fecha +
                 "\r\nHora: " + hora +
-                "\r\nConsultorio: " + consultorio +
-                "\r\nOdontólogo: " + odontologo.Nombre +
-                "\r\nPaciente: " + paciente.Nombre;
+                "\r\nOdontólogo: " + odontologo.Nombre;
+                
         }
-
     }
 }
