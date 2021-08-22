86c3d587f3a65bb68764efc7734716e298efe872
EMILY GISELL GONZALEZ MATEO <emily.gonzalezm@ug.edu.ec>
2021-08-22 11:25:33 -0500

    Odontologo

--- Model/Horario.cs
+++ Model/Horario.cs
@@ -9,16 +9,21 @@ namespace Model {
     public class Horario {
 
         private List<Dias> dias;
+        private string tipo;
 
         public Horario () {
             this.dias = null;
+            this.Tipo = "";
         }
 
-        public Horario (List<Dias> dias) {
+        public Horario (List<Dias> dias, string tipo) {
             this.dias = dias;
+            this.Tipo = tipo;
+            
         }
 
         public List<Dias> Dias { get => dias; set => dias = value; }
+        public string Tipo { get => tipo; set => tipo = value; }
 
         public override string ToString () {
             return base.ToString ();
--- Model/Odontologo.cs
+++ Model/Odontologo.cs
@@ -4,40 +4,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Model {
-    public class Odontologo : Persona, IOdontologo {
+namespace Model
+{
+    public class Odontologo : Persona, IOdontologo
+    {
 
         private string especialidad;
-        private Horario horario;
         private int consultorio;
+        private Horario horario;
+        private int id_Odontologo;
 
-        public Odontologo () {
-            this.especialidad = "";
+
+        public Odontologo()
+        {
+            this.especialidad = " ";
+            this.id_Odontologo = 04;
+            this.consultorio = 1;
             this.horario = null;
-            this.consultorio = 0;
         }
 
-        public Odontologo (string especialidad, Horario horario, int consultorio) {
-            this.especialidad = especialidad;
-            this.horario = horario;
-            this.consultorio = consultorio;
-        }
 
         public string Especialidad { get => especialidad; set => especialidad = value; }
+        public  Horario Horario { get => horario; set => horario = value; }
         public int Consultorio { get => consultorio; set => consultorio = value; }
-        internal Horario Horario { get => horario; set => horario = value; }
+        public int Id_Odontologo { get => id_Odontologo; set => id_Odontologo = value; }
 
-        public string Estado(int consultorio)
+        public Odontologo(int id_Odontologo, string nombre, string cedula, string especialidad, char sexo, DateTime fechaNacimiento, string correo, string telefono, int consultorio) : base(cedula, sexo, nombre, fechaNacimiento, correo, telefono)
         {
-            throw new NotImplementedException();
+            this.Especialidad = especialidad;
+
+            this.Id_Odontologo = id_Odontologo;
+            this.consultorio = consultorio;
+        }
+        public String Estado(int consultorio)
+        {
+            string estado;
+
+            Random r = new Random();
+            int e;
+            e = r.Next(1, 6);
+            if (consultorio == e)
+            {
+                estado = "Disponible";
+            }
+            else
+            {
+                estado = "Ocupado";
+            }
+
+            return estado;
+
         }
 
-        public override string ToString () {
-            return
+
+
+        public override string ToString()
+        {
+            return base.ToString() +
                 "\r\nEspecialidad: " + especialidad +
-                "\r\nConsultorio: " + consultorio +
-                "\r\nHorario: " + horario.ToString ();
-        }
+                "\r\nHorario: " + horario.ToString() +
+                "\r\nEstado: " + Estado(consultorio) +
+                "\r\nConsultorio:" + consultorio;
 
+        }
     }
 }
