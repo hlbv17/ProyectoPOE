4b92380960fe8ab57ce332f4ba325e7c9e41e5b3
Helen Bernal <helenlbv17@gmail.com>
2021-09-13 19:04:08 -0500

    .

--- Visual/FrmEditarCitasHLBV.cs
+++ Visual/FrmEditarCitasHLBV.cs
@@ -1,12 +1,5 @@
 ﻿using Control;
-using Datos;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 
 namespace Visual
--- Visual/FrmEditarVLRS.cs
+++ Visual/FrmEditarVLRS.cs
@@ -2,12 +2,6 @@
 using Datos;
 using Model;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 
 namespace Visual
