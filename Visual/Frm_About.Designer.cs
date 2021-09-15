df838eb72b4aaf3b63dcf5e95e64c202ce5f9198
ropbGitHub <64661058+ropbGitHub@users.noreply.github.com>
2021-09-15 00:41:35 -0500

    About

--- Visual/Frm_About.Designer.cs
+++ Visual/Frm_About.Designer.cs
@@ -24,6 +24,7 @@ namespace Visual {
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent () {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_About));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -33,6 +34,8 @@ namespace Visual {
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -42,7 +45,7 @@ namespace Visual {
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(32, 153);
+            this.label1.Location = new System.Drawing.Point(36, 214);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(92, 13);
             this.label1.TabIndex = 4;
@@ -51,7 +54,7 @@ namespace Visual {
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(249, 153);
+            this.label2.Location = new System.Drawing.Point(253, 214);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(80, 13);
             this.label2.TabIndex = 5;
@@ -60,7 +63,7 @@ namespace Visual {
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(41, 333);
+            this.label3.Location = new System.Drawing.Point(45, 394);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(83, 13);
             this.label3.TabIndex = 6;
@@ -69,7 +72,7 @@ namespace Visual {
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(232, 333);
+            this.label4.Location = new System.Drawing.Point(236, 394);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(107, 13);
             this.label4.TabIndex = 7;
@@ -78,7 +81,7 @@ namespace Visual {
             // pictureBox4
             // 
             this.pictureBox4.Image = global::Visual.Properties.Resources.WhatsApp_Image_2021_09_14_at_23_52_47;
-            this.pictureBox4.Location = new System.Drawing.Point(216, 192);
+            this.pictureBox4.Location = new System.Drawing.Point(220, 253);
             this.pictureBox4.Name = "pictureBox4";
             this.pictureBox4.Size = new System.Drawing.Size(140, 138);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -88,7 +91,7 @@ namespace Visual {
             // pictureBox3
             // 
             this.pictureBox3.Image = global::Visual.Properties.Resources.WhatsApp_Image_2021_09_14_at_23_51_34;
-            this.pictureBox3.Location = new System.Drawing.Point(216, 12);
+            this.pictureBox3.Location = new System.Drawing.Point(220, 73);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(140, 138);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -98,7 +101,7 @@ namespace Visual {
             // pictureBox2
             // 
             this.pictureBox2.Image = global::Visual.Properties.Resources._2020_06_30_09_30_41_1;
-            this.pictureBox2.Location = new System.Drawing.Point(12, 192);
+            this.pictureBox2.Location = new System.Drawing.Point(16, 253);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(140, 138);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -108,7 +111,7 @@ namespace Visual {
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Visual.Properties.Resources.WhatsApp_Image_2021_09_14_at_23_51_05;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(16, 73);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(140, 138);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -118,17 +121,42 @@ namespace Visual {
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(472, 12);
+            this.label5.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(334, 19);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.Size = new System.Drawing.Size(76, 21);
             this.label5.TabIndex = 8;
             this.label5.Text = "Dentalig";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(387, 86);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(391, 170);
+            this.label6.TabIndex = 9;
+            this.label6.Text = resources.GetString("label6.Text");
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(373, 297);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(405, 51);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Facultad de Ciencias Matemáticas y Física, Universidad de Guayaquil\r\nSOF-S-MA-5-1" +
+    ": Programación Orientada a Eventos\r\nIng. Verónica R. Mendoza";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Frm_About
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -162,5 +190,7 @@ namespace Visual {
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
