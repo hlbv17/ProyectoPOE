6f829018dd630b9a63ff546474d9252ffdbfb219
Helen Bernal <helenlbv17@gmail.com>
2021-09-14 23:48:57 -0500

    .

--- Visual/Frm_Menu.cs
+++ Visual/Frm_Menu.cs
@@ -132,5 +132,9 @@ namespace Visual {
             }
         }
 
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
